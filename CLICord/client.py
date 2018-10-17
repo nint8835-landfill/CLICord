d9652ec9833b497440a7e151b15033c846cf1f79
Riley Flynn <riley@rileyflynn.me>
2018-10-17 19:13:38 -0230

    Made UI be closer to what the final UI will be

--- CLICord/client.py
+++ CLICord/client.py
@@ -1,26 +1,37 @@
+import disco.types
 from prompt_toolkit.application import Application
+from prompt_toolkit.buffer import Buffer
+from prompt_toolkit.key_binding import KeyBindings
 from prompt_toolkit.layout import Layout
 from prompt_toolkit.layout.containers import HSplit, Window, WindowAlign
 from prompt_toolkit.layout.controls import FormattedTextControl
-from prompt_toolkit.key_binding import KeyBindings
+from prompt_toolkit.widgets import TextArea, Frame
+
 
 class Client(object):
-
     _app: Application
     _keybindings: KeyBindings
 
     _root_container: HSplit
+    _message_text_area: TextArea
+    _message_input: TextArea
 
-    title_text: str
+    _current_channel: disco.types.Channel
 
     def __init__(self):
-        self.title_text = "CLICord - Not connected"
-        self._root_container = HSplit([
-            Window(height=1,
-                   content=FormattedTextControl(lambda: [("class:title", self.title_text)]), 
-                   align=WindowAlign.CENTER),
-            Window(height=1, char="-", style="class:line")
+        self._current_channel = None
 
+        self._message_text_area = TextArea(read_only=True, focusable=False)
+
+        self._message_input = TextArea(height=3, multiline=False)
+        self._message_input.accept_handler = self._handle_message_entered
+
+        self._root_container = HSplit([
+            Frame(Window(height=1,
+                         content=FormattedTextControl(lambda: [("class:title", self.get_title())]),
+                         align=WindowAlign.CENTER)),
+            self._message_text_area,
+            Frame(self._message_input)
         ])
 
         self._keybindings = KeyBindings()
@@ -31,3 +42,16 @@ class Client(object):
 
         self._app = Application(layout=Layout(self._root_container), full_screen=True, key_bindings=self._keybindings)
         self._app.run()
+
+    def _handle_message_entered(self, buffer: Buffer):
+        self.send_message(buffer.text)
+        buffer.text = ""
+
+    def send_message(self, text):
+        self._message_text_area.text += f"{text}\n"
+
+    def get_title(self):
+        if not self._current_channel:
+            return "No channel selected"
+        else:
+            return f"{self._current_channel.guild.name} > {self._current_channel.name}"
