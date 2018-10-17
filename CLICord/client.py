e8d2d49cf07d5e1aac657df00df05fb1049cd78a
Riley Flynn <riley@rileyflynn.me>
2018-10-17 14:19:41 -0230

    Made app display slightly more relevant (but still useless) info

--- CLICord/client.py
+++ CLICord/client.py
@@ -1,25 +1,33 @@
 from prompt_toolkit.application import Application
 from prompt_toolkit.layout import Layout
-from prompt_toolkit.widgets import Box, Frame, TextArea
+from prompt_toolkit.layout.containers import HSplit, Window, WindowAlign
+from prompt_toolkit.layout.controls import FormattedTextControl
 from prompt_toolkit.key_binding import KeyBindings
 
 class Client(object):
 
-    __slots__ = ["_app", "_layout", "_root_container", "_keybindings"]
     _app: Application
-
-    _layout: Layout
-    _root_container: Box
     _keybindings: KeyBindings
 
+    _root_container: HSplit
+
+    title_text: str
+
     def __init__(self):
-        self._root_container = Box(Frame(TextArea(text="This is a test", width=40, height=3)))
-        self._layout = Layout(container=self._root_container)
+        self.title_text = "CLICord - Not connected"
+        self._root_container = HSplit([
+            Window(height=1,
+                   content=FormattedTextControl(lambda: [("class:title", self.title_text)]), 
+                   align=WindowAlign.CENTER),
+            Window(height=1, char="-", style="class:line")
+
+        ])
+
         self._keybindings = KeyBindings()
 
         @self._keybindings.add("c-c")
         def _(event):
             event.app.exit()
 
-        self._app = Application(layout=self._layout, full_screen=True, key_bindings=self._keybindings)
+        self._app = Application(layout=Layout(self._root_container), full_screen=True, key_bindings=self._keybindings)
         self._app.run()
